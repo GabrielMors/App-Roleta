a6b01c22f8b788cacef893d85c41296989c048c3
Gabriel Mors <gabrielmors210@gmail.com>
2023-06-11 13:01:28 -0400

    [FIX] finalized app, but need to fix a small error

--- Roleta/Home/HomeViewController.swift
+++ Roleta/Home/HomeViewController.swift
@@ -8,8 +8,12 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
     var screen: HomeScreen?
+    var listPerson: [Person] = []
+    var listImage: [String] = ["Image-1", "Image-2", "Image-3", "Image-4", "Image-5"]
+    var person: Person?
+    var alert: AlertController?
     
     override func loadView() {
         screen = HomeScreen()
@@ -24,6 +28,77 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
+        screen?.configTableView(delegate: self, dataSource: self)
+        screen?.textFieldDelegate(delegate: self)
+        blockedDrawNumberButton()
+    }
+    
+    private func blockedDrawNumberButton() {
+        if listPerson.isEmpty {
+            screen?.raffleNumberButton.isEnabled = false
+            screen?.raffleNumberButton.alpha = 0.5
+        } else {
+            screen?.raffleNumberButton.isEnabled = true
+            screen?.raffleNumberButton.alpha = 1
+        }
     }
 }
 
+extension HomeViewController: UITableViewDelegate {
+    
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        if listPerson[indexPath.row] === person {
+//            alert?.showAlert(title: "Muitoo bom", message: "Agora é sua vez, pague a conta 🤪")
+//            listPerson.removeAll()
+//        } else {
+//            alert?.showAlert(title: "Uffa", message: "você escapou dessa vez 🥳")
+//            listPerson.remove(at: indexPath.row)
+//        }
+//        blockedDrawNumberButton()
+//        screen?.tableView.reloadData()
+//    }
+}
+
+extension HomeViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if listPerson.count == 0 {
+            return 1
+        } else {
+            return listPerson.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if listPerson.count == 0 {
+            let cell = screen?.tableView.dequeueReusableCell(withIdentifier: EmptyTableViewCell.identifier) as? EmptyTableViewCell
+            return cell ?? UITableViewCell()
+        } else {
+            let cell = screen?.tableView.dequeueReusableCell(withIdentifier: PersonTableViewCell.identifier, for: indexPath) as? PersonTableViewCell
+            cell?.setupCell(data: listPerson[indexPath.row])
+            return cell ?? UITableViewCell()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if listPerson.count == 0 {
+            return 237
+        } else {
+            return 92
+        }
+    }
+}
+
+extension HomeViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        if !(textField.text?.isEmpty ?? false) {
+            listPerson.append(Person(name: textField.text ?? "", image: listImage.randomElement() ?? ""))
+            screen?.tableView.reloadData()
+            blockedDrawNumberButton()
+        }
+        textField.text = ""
+        return true
+    }
+}
