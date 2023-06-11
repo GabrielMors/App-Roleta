f3aa9d2e73e72821d145a6e8a5ba7f334b2e9f6b
Gabriel Mors <gabrielmors210@gmail.com>
2023-06-11 08:10:02 -0400

    create homeScreen elements

--- Roleta/Home/Screen/HomeScreen.swift
+++ Roleta/Home/Screen/HomeScreen.swift
@@ -9,6 +9,50 @@ import UIKit
 
 class HomeScreen: UIView {
 
+    lazy var logoImageView: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(named: "card")
+        image.contentMode = .scaleAspectFit
+        return image
+    }()
+    
+    lazy var nameTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.font = UIFont.systemFont(ofSize: 18)
+        textField.placeholder = "type your name:"
+        textField.autocorrectionType = .no
+        textField.spellCheckingType = .no
+        textField.keyboardType = .default
+        textField.clearsOnInsertion = true
+        return textField
+    }()
+    
+    lazy var raffleNumberButton: UIButton = {
+       let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Sortear", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.backgroundColor = UIColor.blue
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 8
+        button.addTarget(self, action: #selector(tappedButton), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc private func tappedButton() {
+        
+    }
+    
+    lazy var tableView: UITableView = {
+       let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.showsVerticalScrollIndicator = false
+//        tableView.register(<#T##UINib?#>, forCellReuseIdentifier: <#T##String#>)
+        
+        return tableView
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
