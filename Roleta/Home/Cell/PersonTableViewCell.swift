66294b279855787f551073f25f02599684f66f81
Gabriel Mors <gabrielmors210@gmail.com>
2023-06-11 12:09:36 -0400

    [FIX] created my personCell elements

--- Roleta/Home/Cell/PersonTableViewCell.swift
+++ Roleta/Home/Cell/PersonTableViewCell.swift
@@ -11,11 +11,42 @@ class PersonTableViewCell: UITableViewCell {
 
     static let identifier: String = String(describing: PersonTableViewCell.self)
     
+    lazy var personImageView: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(named: "")
+        image.contentMode = .scaleAspectFit
+        return image
+    }()
+    
+    lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        return label
+    }()
+
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        addSubViews()
+        configConstraints()
+    }
+    
+    private func addSubViews() {
+        addSubview(personImageView)
+        addSubview(nameLabel)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    public func setupCell(data: Person) {
+        nameLabel.text = data.name
+        personImageView.image = UIImage(named: data.image)
+    }
+    
+    private func configConstraints() {
+        
     }
 }
