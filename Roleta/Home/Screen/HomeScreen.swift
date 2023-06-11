42bf4c4fb2c6dbb3e6ffd0499450df0084babd8b
Gabriel Mors <gabrielmors210@gmail.com>
2023-06-11 11:23:16 -0400

    [FIX] setting homeScreen constraints

--- Roleta/Home/Screen/HomeScreen.swift
+++ Roleta/Home/Screen/HomeScreen.swift
@@ -25,7 +25,13 @@ class HomeScreen: UIView {
         textField.autocorrectionType = .no
         textField.spellCheckingType = .no
         textField.keyboardType = .default
+        textField.backgroundColor = .white
+        textField.clipsToBounds = true
+        textField.layer.cornerRadius = 10
         textField.clearsOnInsertion = true
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: textField.frame.height))
+        textField.leftView = paddingView
+        textField.leftViewMode = .always
         return textField
     }()
     
@@ -36,8 +42,9 @@ class HomeScreen: UIView {
         button.setTitleColor(UIColor.white, for: .normal)
         button.backgroundColor = UIColor.blue
         button.clipsToBounds = true
-        button.layer.cornerRadius = 8
+        button.layer.cornerRadius = 10
         button.addTarget(self, action: #selector(tappedButton), for: .touchUpInside)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
         return button
     }()
     
@@ -49,6 +56,7 @@ class HomeScreen: UIView {
        let tableView = UITableView()
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.showsVerticalScrollIndicator = false
+        tableView.backgroundColor = .black
 //        tableView.register(<#T##UINib?#>, forCellReuseIdentifier: <#T##String#>)
         
         return tableView
@@ -56,10 +64,49 @@ class HomeScreen: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        addSubView()
+        configConstraints()
+    }
+    
+    private func addSubView() {
+        addSubview(logoImageView)
+        addSubview(nameTextField)
+        addSubview(raffleNumberButton)
+        addSubview(tableView)
+    }
+    
+    public func configTableView(delegate: UITableViewDelegate, dataSource: UITableViewDataSource) {
+        tableView.delegate = delegate
+        tableView.dataSource = dataSource
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private func configConstraints() {
+        NSLayoutConstraint.activate([
+        
+            logoImageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 30),
+            logoImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 40),
+            logoImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -40),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200),
+            
+            nameTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 15),
+            nameTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            nameTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            nameTextField.heightAnchor.constraint(equalToConstant: 40),
+            
+            raffleNumberButton.topAnchor.constraint(equalTo: nameTextField.bottomAnchor, constant: 15),
+            raffleNumberButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            raffleNumberButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            raffleNumberButton.heightAnchor.constraint(equalToConstant: 40),
+            
+            tableView.topAnchor.constraint(equalTo: raffleNumberButton.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
+            
+        ])
+    }
 }
